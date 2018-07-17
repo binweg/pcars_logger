6f44ed1b7fafb60443026f92a406274ce80cef45
Johann Markl <johann.markl@gmail.com>
2018-07-17 08:21:23 +0200

    log valid laps to csv file

--- logger.py
+++ logger.py
@@ -1,15 +1,19 @@
 import socket
 import struct
+import csv
 from collections import namedtuple
 
 ParticipantInfo = namedtuple('ParticipantInfo',
     ['current_lap_distance', 'current_lap'])
 
-def current_lap_is_invalid(race_state_flags):
-    return bool(race_state_flags & 8)
+SessionState = namedtuple('SessionState',
+    ['current_lap', 'current_lap_time', 'current_lap_invalid'])
+
+def current_lap_is_invalid(race_state):
+    return bool(race_state & 8)
 
 def unpack_float(buffer):
-    return struct.unpack('f', buffer)
+    return struct.unpack('f', buffer)[0]
 
 def unpack_participant_info(buffer):
     p_info = ParticipantInfo(
@@ -18,44 +22,68 @@ def unpack_participant_info(buffer):
     )
     return p_info
 
+def is_new_lap(old_session_state, session_state):
+    return (old_session_state.current_lap_time == -1.0 and session_state.current_lap_time > 0 \
+        or session_state.current_lap == old_session_state.current_lap + 1)
 
-def int_from_bytes(byte_string):
-    return int.from_bytes(byte_string, byteorder='little')
+def int_from_bytes(buffer):
+    return int.from_bytes(buffer, byteorder='little')
 
 pc_port = 5606
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# sock.settimeout(5)
+sock.settimeout(10)
 sock.bind(('', pc_port))
 
 data = b''
-while True:
-    data = sock.recv(2048)
-    if len(data) == 1367: #Telemetry
-        game_state = data[3]
-        race_state_flags = data[10]
-        lap_invalidated = current_lap_is_invalid(race_state_flags)
-        current_lap_time = unpack_float(data[20:24])
-        last_lap_time = unpack_float(data[16:20])
-        participant_info = unpack_participant_info(data[464:464+16])
-        brake = data[112]
-        throttle = data[113]
-        steering = data[115]
-        speed = unpack_float(data[120:124])
-        rpm = int.from_bytes(data[124:126], byteorder='little')
-        max_rpm = int.from_bytes(data[126:128], byteorder='little')
-        gear = data[128] & 0x0f
+old_session_state = SessionState(current_lap=1, current_lap_time=-1, current_lap_invalid=True)
+track_data_rows = []
 
-        print(lap_invalidated,
-            participant_info.current_lap,
-            participant_info.current_lap_distance,
-            gear,
-            rpm,
-            steering,
-            throttle,
-            brake)
-    if len(data) == 1347:
-        pass
+with open('trackdata.csv', 'w', newline='') as track_file:
+    writer = csv.writer(track_file)
+    writer.writerow(['lap', 'pos', 'speed', 'throttle', 'brake', 'steering', 'gear'])
+    while True:
+        data = sock.recv(2048)
+        if len(data) == 1367: #Telemetry
+            game_state = data[3]
+            race_state = data[10]
+            current_lap_invalid = current_lap_is_invalid(race_state)
+            current_lap_time = unpack_float(data[20:24])
+            last_lap_time = unpack_float(data[16:20])
+            participant_info = unpack_participant_info(data[464:464+16])
+            brake = data[112]
+            throttle = data[113]
+            steering = data[115]
+            if steering > 127:
+                steering -= 256
+            speed = unpack_float(data[120:124])
+            rpm = int.from_bytes(data[124:126], byteorder='little')
+            max_rpm = int.from_bytes(data[126:128], byteorder='little')
+            gear = data[128] & 0x0f
+            if gear > 8:
+                gear -= 16
 
-# int.from_bytes(data[470:472], byteorder='little')
-# currentGameState.SessionData.IsNewLap = previousGameState == null || currentGameState.SessionData.CompletedLaps == previousGameState.SessionData.CompletedLaps + 1
+            session_state = SessionState(
+                participant_info.current_lap,
+                current_lap_time,
+                current_lap_invalid)
+
+            if is_new_lap(old_session_state, session_state):
+                if not old_session_state.current_lap_invalid:
+                    writer.writerows(track_data_rows)
+                track_data_rows = []
+
+            old_session_state = session_state
+
+            if not current_lap_invalid:
+                track_data_rows.append((
+                    participant_info.current_lap,
+                    participant_info.current_lap_distance,
+                    speed,
+                    throttle,
+                    brake,
+                    steering,
+                    gear,
+                ))
+        if len(data) == 1347:
+            pass
